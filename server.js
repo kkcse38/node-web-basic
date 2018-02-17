c2004878ca845f92b05ecebf9c085a75da23882f
Kanhaiya <kanhaiyakumar785@gmail.com>
2018-02-17 11:33:41 +0530

    First commit

---
+++ server.js
@@ -0,0 +1,77 @@
+
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+
+var app = express();
+
+hbs.registerPartials(__dirname + '/views/partials');
+app.set('view engine','hbs');
+//app.use(express.static(__dirname + '/views'));
+
+//Middleware
+app.use((req, res, next)=>{
+    //After calling next() middleware allow to response to serverr
+    var date = new Date().toString();
+    var log = `${req.method}: ${req.url}`;
+    fs.appendFile('server.log', log+ '\n', (error)=>{
+        if(error)
+            console.log('Unable to append to server log');
+    });
+    next();
+});
+
+app.use((req, res, next)=>{
+    res.render('maintenance.hbs');
+});
+
+//After writing this statement here, It makes the views file private and not allowed to execute after render
+app.use(express.static(__dirname + '/views'));
+
+hbs.registerHelper('getCurrentYear', ()=>{
+    return new Date().getFullYear()
+});
+
+hbs.registerHelper('screamIt', (message)=>{
+    //message = "kk";
+    return message.toUpperCase();
+});
+//Set http rout handelors.
+//took two arguments
+//First - url
+//Second - Function, which tell what to return as a response
+app.get('/', (req, res)=>{
+    var heck = {
+        pageTitle : 'Hello Express, You are Awesome',
+        location : res.location,
+       // currentYear: new Date().getFullYear(),
+        url: req.url,
+        welcomeMessage: 'welcome message',
+        likes : [
+            'coding',
+            'tetsing',
+        ]
+    };
+    //res.send(heck);
+    res.render('home.hbs', heck);
+});
+
+app.get('/about', (req, res)=>{
+    //res.send('About');
+    res.render('about.hbs', {
+        pageTitle: 'About hbs page!',
+       //currentYear: new Date().getFullYear()
+    });
+});
+
+app.get('/bad', (req, res)=>{
+    res.send({
+        error: '404',
+        status: 'Not Found'
+    });
+});
+
+//We do need to listen app.get by app.listen method
+app.listen(3000, ()=>{
+    console.log('Server listen on 3000');
+});
